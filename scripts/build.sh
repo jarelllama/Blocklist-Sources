1bcd5b8d6c50290b7389ec842c412a59527ac988
J <91372088+jarelllama@users.noreply.github.com>
2025-01-10 17:26:04 +0800

    add easydmarc source

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
-# Retrieve domains labelled as suspicious from Gridinsoft website reputation
-# checker.
+# Retrieves domains from the various sources and builds the respective
+# blocklist.
 
-readonly URL='https://gridinsoft.com/website-reputation-checker'
+readonly DOMAIN_REGEX='[[:alnum:]][[:alnum:].-]*[[:alnum:]]\.[[:alnum:]-]*[a-z]{2,}[[:alnum:]-]*'
 
-build() {
-    # Append new domains
+source_gridinsoft() {
+    local URL='https://gridinsoft.com/website-reputation-checker'
+
     # Some entries have '_' instead of '-' in the domain name
     curl -sS --retry 2 --retry-all-errors "$URL" \
         | grep -Po "online-virus-scanner/url/\K[[:alnum:].-_]+-[[:alnum:]-]+(?=\".*--suspicious\">)" \
-        | mawk '{gsub(/_/, "-"); gsub(/-/, "."); print}' >> suspicious.txt
+        | mawk '{gsub(/_/, "-"); gsub(/-/, "."); print}' >> gridinsoft.txt
 
+    build gridinsoft.txt
+}
+
+source_easydmarc() {
+    local url='https://easydmarc.com/tools/phishing-url'
+
+    curl -sS --retry 2 --retry-all-errors "$url" \
+        | grep -Po "https://\K${DOMAIN_REGEX}(?=(/[^/]+)*</a></td><td><span class=\"eas-tag eas-tag--standard eas-tag--red\">SUSPICIOUS)" \
+        >> easydmarc.txt
+
+    build easydmarc.txt
+}
+
+# Function 'build' formats the blocklist passed.
+# Input:
+#   $1: unformatted blocklist to format
+# Output:
+#   Formatted blocklist
+build() {
     # Compile list. See the list of transformations here:
     # https://github.com/AdguardTeam/HostlistCompiler
     # Note the hostlist compiler removes the previous comments and the Adblock
     # Plus header.
     printf "\n"
-    hostlist-compiler -i suspicious.txt -o compiled.tmp
+    hostlist-compiler -i "$1" -o compiled.tmp
 
     # Remove comments
     sed -i '/!/d' compiled.tmp
@@ -30,17 +50,20 @@ build() {
     #dead-domains-linter -a -i compiled.tmp
 
     # Deploy blocklist
-    append_header
-    cat compiled.tmp >> suspicious.txt
+    append_header "$1"
+    cat compiled.tmp >> "$1"
 }
 
+# Function 'append_header' appends the Adblock Plus header to the blocklist.
+# Input:
+#   $1: blocklist to append header to
 append_header() {
-    cat << EOF > suspicious.txt
+    cat << EOF > "$1"
 [Adblock Plus]
-! Title: Gridinsoft Suspicious Domains Blocklsit
-! Description: Suspicious domains scraped hourly from https://gridinsoft.com/website-reputation-checker and meant for https://github.com/jarelllama/Scam-Blocklist.
-! Homepage: https://github.com/jarelllama/Gridinsoft-Blocklist
-! License: https://github.com/jarelllama/Gridinsoft-Blocklist/blob/main/LICENSE
+! Title: Blocklist to be used as a source for https://github.com/jarelllama/Scam-Blocklist
+! Description: Domains scraped every 30 mins to be used as a source for https://github.com/jarelllama/Scam-Blocklist.
+! Homepage: https://github.com/jarelllama/Blocklist-Sources
+! License: https://github.com/jarelllama/Blocklist-Sources/blob/main/LICENSE
 ! Version: $(date -u +"%m.%d.%H%M%S.%Y")
 ! Expires: 30 minutes
 ! Last modified: $(date -u)
@@ -67,4 +90,6 @@ if ! command -v hostlist-compiler &> /dev/null; then
     npm install -g @adguard/hostlist-compiler > /dev/null
 fi
 
-build
+source_gridinsoft
+
+source_easydmarc
