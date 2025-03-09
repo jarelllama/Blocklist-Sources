84d9e23e408a99d99ac59cfb386323662bfb0631
J <91372088+jarelllama@users.noreply.github.com>
2025-03-09 20:07:04 +0800

    add Tranco toplist and do not format blocklists to Adblock

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,15 +9,23 @@ readonly -a SOURCES=(
     chainabuse
     gridinsoft
     malwareurl
+    nrd
+    tranco
 )
 
 main() {
     local source source_url
     for source in "${SOURCES[@]}"; do
         "source_${source}" || true
+
         cat source_results.tmp >> "${source}.txt"
+
         rm source_results.tmp
-        build
+
+        # Remove carriage return characters, convert to lowercase, sort, and
+        # remove duplicates.
+        mawk '{ gsub("\r", ""); print tolower($0) }' "${source}.txt" \
+            | sort -u -o "${source}.txt"
     done
 }
 
@@ -53,37 +61,34 @@ source_malwareurl() {
         > source_results.tmp
 }
 
-# Format the blocklist.
-build() {
-    # Compile list. See the list of transformations here:
-    # https://github.com/AdguardTeam/HostlistCompiler
-    # Note the hostlist compiler removes the previous comments and the Adblock
-    # Plus header.
-    printf "\n"
-    hostlist-compiler -i "${source}.txt" -o compiled.tmp
+source_nrd() {
+    #TODO
+    :
+}
 
-    # Sort as the hostlist compiler does not sort
-    sort -u compiled.tmp -o compiled.tmp
+source_tranco() {
+    local source_url_1='https://tranco-list.eu/top-1m.csv.zip'
+    local source_url_2='https://tranco-list.eu/top-1m-incl-subdomains.csv.zip'
+    local max_attempts=3  # Retries twice
+    local attempt=1
 
-    # Remove comments
-    sed -i '/!/d' compiled.tmp
+    while (( attempt <= max_attempts )); do
+        # Download the toplists in parallel
+        curl -sSL "$source_url_1" -o toplist1.zip &
+        curl -sSL "$source_url_2" -o toplist2.zip
 
-    # Append header
-    cat << EOF > "${source}.txt"
-[Adblock Plus]
-! Title: Blocklist to be used as a source for https://github.com/jarelllama/Scam-Blocklist
-! Description: Blocklist to be used as a source for https://github.com/jarelllama/Scam-Blocklist. Not to be used by itself.
-! Homepage: https://github.com/jarelllama/Blocklist-Sources
-! License: https://github.com/jarelllama/Blocklist-Sources/blob/main/LICENSE
-! Version: $(date -u +"%m.%d.%H%M%S.%Y")
-! Expires: 10 minutes
-! Last modified: $(date -u)
-! Syntax: Adblock Plus
-! Number of entries: $(wc -l < compiled.tmp)
-!
-EOF
+        {
+            unzip -p toplist1.zip
+            unzip -p toplist2.zip
+        } | mawk -F ',' '{ print $2 }' > source_results.tmp
 
-    cat compiled.tmp >> "${source}.txt"
+        rm toplist*.zip
+
+        # Break out of loop if the toplists downloaded successffully.
+        (( $(wc -l < source_results.tmp) == 2000000 )) && break
+
+        (( attempt++ ))
+    done
 }
 
 # Entry point
@@ -91,11 +96,6 @@ EOF
 set -e
 
 trap 'rm ./*.tmp temp 2> /dev/null || true' EXIT
-
-# Install AdGuard's Dead Domains Linter
-if ! command -v dead-domains-linter &> /dev/null; then
-    npm install -g @adguard/dead-domains-linter > /dev/null
-fi
 
 # Install AdGuard's Hostlist Compiler
 if ! command -v hostlist-compiler &> /dev/null; then
